a7da99a4df7eaae728ee4364627b8ff6a39a5d17
intae <kangit2013@gmail.com>
2020-05-25 22:17:45 +0900

    지뢰찾기 3일차, 버그 수정, css 변경

--- Minesweeper/mine.js
+++ Minesweeper/mine.js
@@ -1,9 +1,11 @@
 //sprite로 작은 이미지 
 //백그라운드 포지션을 sprite로 필요한 부분만 보이게끔
 const execBtn = document.querySelector("#exec");
-const tbody = document.querySelector("#table tbody");
+const tbody = document.querySelector("#table > tbody");
 let stopFlag = false;
-
+//지뢰찾기 theat tr 반응형으로 가로 크기 변하게 하기 위해서
+const table = document.querySelector("table")
+const theadTr = table.children[0].children[0];
 
 let HOR, VER, MINE;
 let dataset = [];//지뢰찾기 데이터
@@ -30,20 +32,24 @@ function handleContextMenu(e){//마우스 오른쪽 클릭 물음표
     // console.log(e.target)
     // console.log(e.target.parentNode);
     // console.log(e.target.parentNode.parentNode)
-    const idArr = idRowCol(e.target.id);
-    const row = idArr[0], col = idArr[1];
+
+    // const idArr = idRowCol(e.target.parentNode.id);
+    // const row = idArr[0], col = idArr[1];
+
     // const row = e.target.id[0], col = e.target.id[1];
-    
-    if(e.target.textContent === '' || e.target.textContent === "X"){
+    // console.log(e.target)
+    if(e.target.textContent === ''){
+    // if(e.target.textContent === '' || e.target.textContent === "X"){
         e.target.textContent = "!"
     } else if(e.target.textContent === "!"){
         e.target.textContent = "?"
     } else if(e.target.textContent === "?"){
-        if(dataset[row][col] !== "X"){
-            e.target.textContent = "";
-        } else {
-            e.target.textContent = "X";
-        }
+        e.target.textContent = "";
+        // if(dataset[row][col] !== "X"){
+        //     e.target.textContent = "";
+        // } else {
+        //     e.target.textContent = "X";
+        // }
     }
 }
 function clickZreo(zero, row, col){//지뢰 수 0 클릭시 주변 오픈
@@ -84,50 +90,66 @@ function idRowCol(strId){//문자열형태의 id를 원하는 형태, 숫자인 
     return strId.split(',').map(v => Number(v));
 }
 function handleClick(e){
-    e.preventDefault();
+    // e.preventDefault();
     if(stopFlag){
         return;
     }
- 
-    const idArr = idRowCol(e.target.id);
-    const row = idArr[0], col = idArr[1];
-    // const row = Number(e.target.id[0]), col = Number(e.target.id[1]);
-    
-    if(dataset[row][col] === "X"){
-        e.target.textContent = '펑';
-        stopFlag = true; //클릭 안되게 함
+    let row, col;
+    let idArr = idRowCol(e.target.parentNode.id);
+    if(idArr.length === 1){//가끔 div가 비어 있는 버그 해결책
+        idArr = idRowCol(e.target.id)
+        row = idArr[0];
+        col = idArr[1];
     } else {
-        console.log(dataset)
-        console.log(typeof(dataset[row][col]))
-        e.target.textContent = dataset[row][col];
-        // const r = e.target.parentNode.parentNode.childNodes;
-        // const c = e.target.parentNode.childNodes;
-        
-        // console.log(tbody.childNodes[row-1].childNodes[col-1]);
-        // e.target.parentNode.parentNode.childNodes[row-1].childNodes[col-1].textContent = dataset[row-1][col-1];
-        // console.log(e.target.parentNode.parentNode.childNodes[row-1].childNodes[col-1]);
-        // console.log(e.target.parentNode.parentNode.childNodes[row-1].childNodes[col]);
-        // console.log(e.target.parentNode.parentNode.childNodes[row-1].childNodes[col+1]);
-        // console.log(e.target.parentNode.parentNode.childNodes[row].childNodes[col-1]);
-        // console.log(e.target.parentNode.parentNode.childNodes[row].childNodes[col]);
-        // console.log(e.target.parentNode.parentNode.childNodes[row].childNodes[col+1]);
-        // console.log(e.target.parentNode.parentNode.childNodes[row+1].childNodes[col-1]);
-        // console.log(e.target.parentNode.parentNode.childNodes[row+1].childNodes[col]);
-        // console.log(e.target.parentNode.parentNode.childNodes[row+1].childNodes[col+1]);
-       
-        // tbody.childNodes[row-1].childNodes[col-1].textContent = dataset[row-1][col-1];
-        // tbody.childNodes[row-1].childNodes[col].textContent = dataset[row-1][col];
-        // tbody.childNodes[row-1].childNodes[col+1].textContent = dataset[row-1][col+1];
-        // tbody.childNodes[row].childNodes[col-1].textContent = dataset[row][col-1];
-        // // tbody.childNodes[row].childNodes[col].textContent = dataset[row][col];
-        // tbody.childNodes[row].childNodes[col+1].textContent = dataset[row][col+1];
-        // tbody.childNodes[row+1].childNodes[col-1].textContent = dataset[row+1][col-1];
-        // tbody.childNodes[row+1].childNodes[col].textContent = dataset[row+1][col];
-        // tbody.childNodes[row+1].childNodes[col+1].textContent = dataset[row+1][col+1];
-
-        clickZreo(dataset[row][col], row, col);//지뢰 수 0 클릭시 주변 오픈
+        row = idArr[0];
+        col = idArr[1];
     }
+
+
+    // const row = Number(e.target.id[0]), col = Number(e.target.id[1]);
+    // console.dir(e.target.parentNode)
+    // console.log(typeof(row), col)
     // console.log(dataset)
+
+    if(e.target.textContent === ''){
+        if(dataset[row][col] === "X"){
+        
+            e.target.textContent = '펑';
+            stopFlag = true; //클릭 안되게 함
+        } else {
+            // console.log(dataset)
+            // console.log(typeof(dataset[row][col]))
+            e.target.textContent = dataset[row][col];
+            // const r = e.target.parentNode.parentNode.childNodes;
+            // const c = e.target.parentNode.childNodes;
+            
+            // console.log(tbody.childNodes[row-1].childNodes[col-1]);
+            // e.target.parentNode.parentNode.childNodes[row-1].childNodes[col-1].textContent = dataset[row-1][col-1];
+            // console.log(e.target.parentNode.parentNode.childNodes[row-1].childNodes[col-1]);
+            // console.log(e.target.parentNode.parentNode.childNodes[row-1].childNodes[col]);
+            // console.log(e.target.parentNode.parentNode.childNodes[row-1].childNodes[col+1]);
+            // console.log(e.target.parentNode.parentNode.childNodes[row].childNodes[col-1]);
+            // console.log(e.target.parentNode.parentNode.childNodes[row].childNodes[col]);
+            // console.log(e.target.parentNode.parentNode.childNodes[row].childNodes[col+1]);
+            // console.log(e.target.parentNode.parentNode.childNodes[row+1].childNodes[col-1]);
+            // console.log(e.target.parentNode.parentNode.childNodes[row+1].childNodes[col]);
+            // console.log(e.target.parentNode.parentNode.childNodes[row+1].childNodes[col+1]);
+           
+            // tbody.childNodes[row-1].childNodes[col-1].textContent = dataset[row-1][col-1];
+            // tbody.childNodes[row-1].childNodes[col].textContent = dataset[row-1][col];
+            // tbody.childNodes[row-1].childNodes[col+1].textContent = dataset[row-1][col+1];
+            // tbody.childNodes[row].childNodes[col-1].textContent = dataset[row][col-1];
+            // // tbody.childNodes[row].childNodes[col].textContent = dataset[row][col];
+            // tbody.childNodes[row].childNodes[col+1].textContent = dataset[row][col+1];
+            // tbody.childNodes[row+1].childNodes[col-1].textContent = dataset[row+1][col-1];
+            // tbody.childNodes[row+1].childNodes[col].textContent = dataset[row+1][col];
+            // tbody.childNodes[row+1].childNodes[col+1].textContent = dataset[row+1][col+1];
+    
+            clickZreo(dataset[row][col], row, col);//지뢰 수 0 클릭시 주변 오픈
+        }
+        // console.log(dataset)
+    }
+    
 }
 
 function createMineTable(hor, ver){//지뢰찾기 테이블 만들기
@@ -139,9 +161,15 @@ function createMineTable(hor, ver){//지뢰찾기 테이블 만들기
         for(let j = 0; j < hor; j++){
             arr.push(0);
             let td = document.createElement("td");
+            const tdDiv = document.createElement("div");//td 최소크기 설정하기 위해
+            tdDiv.id = "tdDiv";
+            td.appendChild(tdDiv);
+
             td.addEventListener("contextmenu", handleContextMenu);//마우스 오른쪽 클릭시 물음표
             td.addEventListener("click", handleClick);
+
             td.id = i+''+','+j+'';//table에서 td 좌표 알기 위해 id설정
+            
             tr.appendChild(td);
             // td.textContent = 1;
         }
@@ -208,22 +236,42 @@ function planingMines(hor, ver, mine){//지뢰심기
         let mineHor = arr[1];
         
         // let mineHor = minesPosition[k] % 10;//가로
-        tbody.children[mineVer].children[mineHor].textContent = "X";
+        // tbody.childNodes[mineVer].childNodes[mineHor].textContent = "X";
+        // tbody.children[mineVer].children[mineHor].textContent = "X";
         // tbody.children[mineVer].children[mineHor].textContent = mineVer+''+mineHor+'';
         dataset[mineVer][mineHor]  = "X";
         
         countMines(mineVer, mineHor);
     }
+}
+function theadAddDiv(){//thead에 깃발수 시간 이모티콘 넣기 위해
+    console.dir(theadTr)
+    const theadTh = theadTr.children[0];
+    console.log(theadTh);
+    console.dir(theadTh);
+    const theadThContainer = document.createElement("div");
+    const flagDiv = document.createElement("div");
+    const emotionDiv = document.createElement("div");
+    const timerDiv = document.createElement("div");
+    theadThContainer.append(flagDiv, emotionDiv, timerDiv);
+    theadTh.append(theadThContainer);
 
-    
 }
 
 function execClick(e){//실행 버튼 클릭시
     // e.preventDefault();
-    HOR = parseInt(document.querySelector("#hor").value);//가로
-    VER = parseInt(document.querySelector("#ver").value);//세로
-    MINE = parseInt(document.querySelector("#mine").value);
+    // HOR = parseInt(document.querySelector("#hor").value);//가로
+    // VER = parseInt(document.querySelector("#ver").value);//세로
+    // MINE = parseInt(document.querySelector("#mine").value);
     
+    HOR = 10;
+    VER = 10;
+    MINE = 20;
+    // theadTr.innerHTML = "<th colspan=" + `${HOR}`+ "></th>";
+    theadTr.innerHTML = "<th colspan=" + HOR+ "></th>";
+    theadAddDiv();//thead에 깃발수 시간 이모티콘 넣기 위해
+    //thead tr 반응형으로 가로 크기 변하게 하기 위해서
+
     createMineTable(HOR, VER);//지뢰찾기 테이블 만들기
     planingMines(HOR, VER, MINE);//지뢰 심기
 
@@ -238,7 +286,8 @@ function execClick(e){//실행 버튼 클릭시
 // }
 
 function init(){
-    execBtn.addEventListener("click", execClick);//실행 버튼 클릭시
+    // execBtn.addEventListener("click", execClick);//실행 버튼 클릭시
+    execClick();
     
 }
 init();
